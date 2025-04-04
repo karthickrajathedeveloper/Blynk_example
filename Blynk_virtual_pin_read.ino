b3c6ba7ddf585aa76df74a2c5225ed495db94fb1
Karthickraja <130274513+karthickrajathedeveloper@users.noreply.github.com>
2025-04-04 15:00:07 +0530

    Create Blynk_virtual_pin_read.ino
    Read the Virtual pin using ESP32

---
+++ Blynk_virtual_pin_read.ino
@@ -0,0 +1,38 @@
+#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
+#define BLYNK_TEMPLATE_NAME         "Device"
+#define BLYNK_AUTH_TOKEN            "YourAuthToken"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+
+BLYNK_WRITE(V1)
+{
+  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+  // process received value
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+ 
+}
+
+void loop()
+{
+  Blynk.run();
+}
